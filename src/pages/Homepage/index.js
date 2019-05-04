7cbf35974ba16793a3ba71373b2decb3a37564f2
Weoi En Sng <w.sng2@student.unimelb.edu.au>
2019-05-05 02:43:40 +1000

    links added

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -14,79 +14,76 @@ class Homepage extends Component {
   render() {
     return (
 
-
       <div className='d-flex justify-content-center'>
-
-
           <Container>
-
                   <div className="row">
-
                       <p className='text-main'>
                           Hackathons coming soon.
-
                       </p>
-
-
                   </div>
-
-                  <Row>
-
-
-
-                        <Card style={{ width: '18rem' }}>
+                  <Row className='card-row'>
+                        <Card className = "hack-card" style={{ width: '18rem' }}>
                           <Card.Img variant="top" src="https://scontent-syd2-1.xx.fbcdn.net/v/t1.0-9/12049559_10153048358195988_6195809242732784095_n.png?_nc_cat=107&_nc_ht=scontent-syd2-1.xx&oh=b6e9335aeacfa0ceaa12f00098ec939a&oe=5D69F297" />
                           <Card.Body>
                             <Card.Title>Facebook Hackathon</Card.Title>
+
                             <Card.Text>
-                              <text>Hackathons are a big tradition at Facebook. They serve as the foundation for some great (and not so great) ideas. It gives our employees the opportunity to try out new ideas and collaborate with other people in a fun environment.
- </text>
+                              <text>Hackathons are a big tradition at Facebook. They serve as the foundation for some great (and not so great) ideas. It gives our employees the opportunity to try out new ideas and collaborate with other people in a fun environment.</text>
                             </Card.Text>
+
                             <div className = 'button'>
-                            <Button variant="primary">Read more</Button>
-                            <div className = 'button'>
-                            </div>
-                            <Button variant="primary">Find teammates</Button>
+                              <Button variant="primary" href="https://www.facebook.com/hackathon/">Read more</Button>
                             </div>
 
+                            <div className = 'button'>
+                              <Button variant="primary" href="../Profiles/index.js">Find t eammates</Button>
+                            </div>
                           </Card.Body>
                         </Card>
 
                   </Row>
 
-                  <Row>
+                  <Row className='card-row'>
 
-
-                      <div className='d-flex justify-content-center'>
-                        <Card style={{ width: '18rem' }}>
+                        <Card className = "hack-card" style={{ width: '18rem' }}>
                           <Card.Img variant="top" src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F60263824%2F297198274436%2F1%2Foriginal.20190412-074944?w=800&auto=compress&rect=0%2C45%2C1594%2C797&s=85a858dbf7a4d784aaf70e48b3076a5b" />
                           <Card.Body>
                             <Card.Title>MEDHACK</Card.Title>
                             <Card.Text>
                               <text>Melbourne’s premier student run Healthcare Innovation hackathon. Every year, it brings students together at the intersection of design, engineering, healthcare and entrepreneurship in a 48 hour sprint to help reimagine the future of healthcare. </text>
                             </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <div className = 'button'>
+                              <Button variant="primary" href="https://medhack.com.au">Read more</Button>
+                            </div>
+                            <div className = 'button'>
+                              <Button variant="primary">Find teammates</Button>
+                            </div>
                           </Card.Body>
                         </Card>
-                      </div>
+
 
 
                     </Row>
-                    <Row>
+                    <Row className='card-row'>
 
 
-                      <div className='d-flex justify-content-center'>
-                        <Card style={{ width: '18rem' }}>
+
+                        <Card className = "hack-card" style={{ width: '18rem' }}>
                           <Card.Img variant="top" src="https://imaginecup.microsoft.com/en-us/Image/SubscriptionProfile/12" />
                           <Card.Body>
                             <Card.Title>Imagine Cup</Card.Title>
                             <Card.Text>
                               <text>Imagine Cup is a global competition that empowers the next generation of computer science students to team up and use their creativity, passion and knowledge of technology to create applications that shape how we live, work and play. Every year tens of thousands of students from across the globe compete for cash, travel and prizes and for the honor of taking home the Imagine Cup!</text>
                             </Card.Text>
-                            <Button variant="primary">Go somewhere</Button>
+                            <div className = 'button'>
+                              <Button variant="primary" href="https://imaginecup.microsoft.com">Read more</Button>
+                            </div>
+                            <div className = 'button'>
+                              <Button variant="primary">Find teammates</Button>
+                            </div>
                           </Card.Body>
                         </Card>
-                      </div>
+
 
 
                   </Row>
@@ -99,5 +96,6 @@ class Homepage extends Component {
     );
   }
 }
+
 
 export default Homepage;
