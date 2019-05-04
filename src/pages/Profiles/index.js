814110aa90fc1b2eba38e061e1f8c22b63a2bad2
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2019-05-05 03:58:15 +1000

    added swipe

--- src/pages/Profiles/index.js
+++ src/pages/Profiles/index.js
@@ -1,30 +1,112 @@
 import React, { Component, Fragment } from 'react';
-import Cards, { Card } from 'react-swipe-card';
+import { Card } from 'react-bootstrap';
 
-const getUsers = () => {
-  return [
-    {
-      name: 'Umair',
-      skills: [
+import users from '../../data/users';
 
-      ]
-    },
-    {
-      name: 'Isitha',
-      skills: [
+import './profiles.scss';
+class SwipeHolder extends Component {
 
-      ]
+  constructor(props) {
+    super(props);
+    // if(this.props.accepted === undefined) {
+    //   this.props.accepted = [];
+    // }
+    // if(this.props.denied === undefined) {
+    //   this.props.denied = [];
+    // }
+    this.elementRef = React.createRef();
+    this.state = {
+      currentX: 0,
+      startX: 0,
+      startTime: 0,
+      endX: 0,
+      endTime: 0,
+      index: 0,
     }
-  ];
-};
+  
+  }
+  
+  componentDidMount() {
+    
+    console.log(this.elementRef);
+    document.addEventListener('touchstart', this.onTouchStart);
+    document.addEventListener('touchend', this.onTouchEnd);
+    document.addEventListener('touchmove', this.onTouchMove);
+    
+  }
+
+  onTouchMove = (e) => {
+    this.setState({currentX: e.touches[0].clientX});
+  }
+
+  onTouchStart = (e) => {
+    //console.log(e);
+    this.setState({startX: e.touches[0].clientX, startTime: e.timeStamp});
+    
+  }
+
+  onTouchEnd = (e) => {
+
+      console.log(e.touches.length > 0);
+      this.setState({endX: e.changedTouches[0].clientX, endTime: e.timeStamp});
+      console.log((this.state.endTime - this.state.startTime));
+      if( (this.state.endTime - this.state.startTime) < 300) {
+        if((this.state.endX-this.state.startX) > 0) {
+          console.log('swipe right');
+          this.handleRightSwipe();
+        } else {
+          console.log('swipe left');
+          this.handleLeftSwipe();
+        }     
+      }else {
+        this.setState({currentX: 0, startX: 0});
+      }
+    
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  resetState = () => {
+    setTimeout(()=>{this.setState({currentX: 0, startX: 0})}, 300);
+  }
+  handleLeftSwipe() {
+    this.setState({index: this.state.index+1});
+    this.resetState();
+  }
+
+  handleRightSwipe() {
+    this.setState({index: this.state.index+1});
+    this.resetState();
+  }
+
+  render() {
+    if(users.length-1 < this.state.index) {
+      return (
+        <div/>
+      );
+    }
+    return (
+      <div ref={this.elementRef} className="swipeable-main">
+        
+        <div style={{position: 'absolute', left: this.state.currentX-this.state.startX, background: 'white', width: '100%'}} className="test">
+          <Card>
+            <Card.Body>
+              {users[this.state.index].name}
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    );
+  }
+}
 
 class Profiles extends Component {
   render() {
     return (
       <Fragment>
-        <Cards >
-          
-        </Cards>
+        <SwipeHolder />
       </Fragment>
     );
   }
